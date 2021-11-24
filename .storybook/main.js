b34d6113c91fe83c4b88bcb14df8cc2a4e7957c8
igara <igara1119@gmail.com>
2021-11-24 20:08:26 +0900

    modify

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,23 @@
+const path = require("path");
+
 module.exports = {
-  stories: ["../projects/syonet_eight_www/components/**/*.stories.mdx"],
+  stories: [
+    "../projects/syonet_eight_www/components/pages/index/*.stories.mdx",
+  ],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "storybook-css-modules-preset",
   ],
+  webpackFinal: async (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@www": path.resolve(__dirname, "../projects/syonet_eight_www"),
+      "@design_system": path.resolve(
+        __dirname,
+        "../projects/syonet_eight_design_system"
+      ),
+    };
+    return config;
+  },
 };
