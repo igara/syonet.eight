507f3f8b12e88821a5b103c35da45ba9e07cf711
igara <igara1119@gmail.com>
2021-12-26 02:55:21 +0900

    add: storybook addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,14 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  stories: ["../projects/**/components/**/*.stories.mdx"],
+  stories: ['../projects/**/components/**/*.stories.mdx'],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-a11y",
-    "@storybook/addon-postcss",
-    "storybook-addon-performance/register",
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-a11y',
+    '@storybook/addon-postcss',
+    'storybook-addon-performance/register',
+    'storybook-addon-mock/register',
   ],
   typescript: {
     reactDocgen: false,
@@ -15,19 +16,10 @@ module.exports = {
   webpackFinal: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
-      "@www": path.resolve(__dirname, "../projects/syonet_eight_www"),
-      "@design_system": path.resolve(
-        __dirname,
-        "../projects/syonet_eight_design_system"
-      ),
-      "@emotion/core": path.resolve(
-        __dirname,
-        "../node_modules/@emotion/react"
-      ),
-      "emotion-theming": path.resolve(
-        __dirname,
-        "../node_modules/@emotion/react"
-      ),
+      '@www': path.resolve(__dirname, '../projects/syonet_eight_www'),
+      '@design_system': path.resolve(__dirname, '../projects/syonet_eight_design_system'),
+      '@emotion/core': path.resolve(__dirname, '../node_modules/@emotion/react'),
+      'emotion-theming': path.resolve(__dirname, '../node_modules/@emotion/react'),
     };
 
     return config;
